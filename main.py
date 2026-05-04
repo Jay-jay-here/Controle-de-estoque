96a7af97d86f44069a804e4478bb123b6042de05
kkkkkkkk802 <josuesamuel290@gmail.com>
2026-05-04 12:15:26 -0300

    isso add o menu principal

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('oi')
+
+print('oi, bem vindo ao nosso projeto de repositor atacadao, pfvr usuario escolha umas desses opcoes a baixo a sua escolha')
+print('1 usuario ')
+print('2 produtos ')
+print('3 estoque ')
+print("4 sair (obs:nao saia, estou te vendo")
+opcao=input('escolhe ai mano ')
+if opcao=="1":
+  print('obaaa opcao 1 ')
+elif opcao=="2":
+   print(' boa usuartio vc escolheu a opcao 2 ')
